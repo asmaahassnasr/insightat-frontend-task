3cefc01944b5958e61918d59dc06c226f5ed0445
asmaahassnasr <asmaa14has@gmail.com>
2020-12-22 20:58:30 +0200

    Add privacy page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Route,Switch} from 'react-router-dom';
 import {Home} from './pages/home/home';
 import {ContactUs} from './pages/contactUs/contactUs';
-import {HowItWorks} from './pages/howItWorks/howItWorks';
+import {Privacy} from './pages/privacy/privacy';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
 
@@ -10,7 +10,7 @@ function App() {
   return (   
     <React.Fragment>
       <Switch>
-      <Route exact path="/" component={HowItWorks}/>
+      <Route exact path="/" component={Privacy}/>
       </Switch>
     </React.Fragment>
   );
