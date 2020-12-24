69dbe644c9f083832cd4fc3807bed898a392630d
asmaahassnasr <asmaa14has@gmail.com>
2020-12-24 05:32:50 +0200

    Add text Question component from survey js and repalce its HTML

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -37,7 +37,7 @@ function App() {
       <Route  path="/editProfile" component={EditProfile}/>
       <Route path="/design" component={SurveyDesign} />
       </Switch>
-      <Footer />
+      {/* <Footer /> */}
     </React.Fragment>
   );
 }
---
+++ my-app/src/pages/survey/components/textQuestion.tsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import * as Survey from "survey-react";
+import "survey-react/survey.css";
+interface IProps{
+    thePlaceHolder:string,
+    theTitle:string
+}
+export const TextQuestion:React.SFC<IProps>  = ({theTitle,thePlaceHolder}) =>{
+    const json = {
+        questions: [
+            {
+                name: "name",
+                type: "text",
+                title: theTitle,
+                placeHolder: thePlaceHolder,
+                autoComplete: "name"
+            }
+        ]
+    };
+        return (
+            <>
+                 <Survey.Survey json={json} 
+                 showNavigationButtons={false} 
+                 />; 
+            </>
+        )
+}
+
