4cd9d96007cbc3134e6271527e05f4dda4781b26
asmaahassnasr <asmaa14has@gmail.com>
2021-01-21 02:51:58 +0200

    Edit Build page style

---
+++ src/pages/faq/faq.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {NavigationBar} from '../MainNavBar/navigationBar';
+
+import './faq.css'; 
+
+
+export const FAQ = () => {
+    return (
+        <>
+        <NavigationBar />
+        <div className="container">
+    <br />
+    <br />
+    
+
+    <br />
+
+    <div className="" id="accordion">
+        <div className="faqHeader">FAQ | Frequently Asked Questions</div>
+        <div className="card ">
+            <div className="card-header">
+                <h4 className="card-header">
+                    <a className="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">What Is Insightat?</a>
+                </h4>
+            </div>
+         
+            <div id="collapseOne" className="panel-collapse collapse in">
+                <div className="card-block">
+                     Paperform Is An Online Platform That Enables Anyone To Create Online Forms Or 
+                     Product Pages Quickly, Intuitively, And To Brand Them As They Like, All Without 
+                     Writing Code. Whether You’re Involved Involved With Organising Events, Sporting 
+                     Groups, Marketing Agencies, Tech Startups, Or Any Other Project, Paperform Is A 
+                     Beautiful.
+                 </div>
+            </div>
+        </div>
+       
+</div>
+</div>
+
+        </>
+     )
+}
