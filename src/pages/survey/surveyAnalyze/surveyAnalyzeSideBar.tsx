d65b55315fa1feeee38906034212a65ac1141fbf
asmaahassnasr <asmaa14has@gmail.com>
2021-01-24 13:31:00 +0200

    Analyze sidebar link

--- src/pages/survey/surveyAnalyze/surveyAnalyzeSideBar.tsx
+++ src/pages/survey/surveyAnalyze/surveyAnalyzeSideBar.tsx
@@ -1,17 +1,19 @@
+import {Link} from 'react-router-dom';
 import './surveyAnalyzeSideBar.css';
+
 export const SurveyAnalyzeSideBar = () => {
     return (
         <>
         <div className="col-lg-2 ">
        <div className="analyze-link">
         <div className="analyze-link-box active-link">
-          <p><a href="#">Status</a></p>
+          <p><Link to="/analyze"><a>Status</a></Link></p>
         </div>
         <div className="analyze-link-box">
-          <p><a href="#">Results</a></p>
+          <p><Link to="/dashboard"><a>Results</a></Link></p>
         </div>
         <div className="analyze-link-box">
-          <p><a href="#">Individual Responses</a></p>
+          <p><a>Individual Responses</a></p>
         </div>
        </div>  
      </div>
