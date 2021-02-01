5140072012c04281e7a55e5945a5e69aaa46ea89
asmaahassnasr <asmaa14has@gmail.com>
2021-02-02 01:56:40 +0200

    solutions page restyle

--- src/pages/solutions/solutions.tsx
+++ src/pages/solutions/solutions.tsx
@@ -4,77 +4,68 @@ import icon_form from '../assets/image/icon_form.png';
 import icon_custom_support from '../assets/image/icon_custom_support.png';
 import icon_product from '../assets/image/icon_product.png';
 import icon_feedback from '../assets/image/icon_feedback.png';
-// import './solutions.css';
+import {Footer} from '../footer/footer';
 import { NavigationBar } from '../MainNavBar/navigationBar';
 
 export const Solutions = () => {
 	return (
-		<div>
+		<>
 			<NavigationBar />
-			<div className="solutions container">
-				<div className="solutions__item">
-					<div className="solutions__item__description">
-						<h1>Not Your Usual Survey and Form Builder</h1>
-						<p>
-							{' '}
-							Build surveys and forms, share via verity of channels and get the results. <br />That’s
-							simple!
-						</p>
+			<div className="container">
+				<div className=" solutions-container">
+				<div className="solution-box">
+					<div className="solution-box-data fl-left">
+						<h4>Not Your Usual Survey and Form Builder</h4>
+						<p>Build surveys and forms, share via verity of channels and get the results.That’s simple!</p>
 					</div>
-					<div className="solutions__item__img">
-						<img src={icon_form} />
+					<div className="solution-box-img fl-right">
+						<img className=" fl-right" src={icon_form} />
 					</div>
 				</div>
-
-				<div className="solutions__item">
-					<div className="solutions__item__img">
-						<img src={icon_custom_support} />
+				<div className="clear-fix" ></div>
+				<div className="solution-box">
+					<div className="solution-box-data fl-right">
+						<h4>The Real Voice of Your Customers</h4>
+						<p>Listen to your customers and show them that you care. Insightat helps you build your own customer satisfaction surveys, or you could pick one of many templates</p>
 					</div>
-
-					<div className="solutions__item__description">
-						<h1>The Real Voice of Your Customers</h1>
-						<p>
-							{' '}
-							Listen to your customers and show them that you care. Insightat helps you build your own
-							customer satisfaction surveys, or you could pick one of many templates.
-						</p>
+					<div className="solution-box-img fl-left">
+						<img className=" fl-left" src={icon_custom_support} />
 					</div>
 				</div>
-
-				<div className="solutions__item">
-					<div className="solutions__item__description">
-						<h1>Employees Engagement Never Been More Professional</h1>
-						<p> Collet the employees' feedback and improve their engagement</p>
+				<div className="clear-fix" ></div>
+				<div className="solution-box">
+					<div className="solution-box-data fl-left">
+						<h4>Employees Engagement Never Been More Professional</h4>
+						<p>Collet the employees' feedback and improve their engagement</p>
 					</div>
-					<div className="solutions__item__img">
-						<img src={icon_employer} />
-					</div>
-				</div>
-
-				<div className="solutions__item">
-					<div className="solutions__item__img">
-						<img src={icon_product} />
-					</div>
-
-					<div className="solutions__item__description">
-						<h1>Improve Your Products And Services</h1>
-						<p>
-							Give your customers the opportunity to rate your services and products for the continues
-							improving.
-						</p>
+					<div className="solution-box-img fl-right">
+						<img className="fl-right" src={icon_employer} />
 					</div>
 				</div>
-
-				<div className="solutions__item">
-					<div className="solutions__item__description">
-						<h1>Instant Feedback (Kiosk)</h1>
+				<div className="clear-fix" ></div>
+				<div className="solution-box">
+					<div className="solution-box-data fl-right">
+						<h4>Improve Your Products And Services</h4>
+						<p>Give your customers the opportunity to rate your services and products for the continues improving</p>
+					</div>
+					<div className="solution-box-img fl-left">
+						<img className="fl-left" src={icon_product} />
+					</div>
+				</div>
+				<div className="clear-fix" ></div>
+				<div className="solution-box">
+					<div className="solution-box-data fl-left">
+						<h4>Instant Feedback (Kiosk)</h4>
 						<p>Get the instant feedback and close the gap between you and the customers.</p>
 					</div>
-					<div className="solutions__item__img">
-						<img src={icon_feedback} />
+					<div className="solution-box-img fl-right">
+						<img className="fl-right" src={icon_feedback} />
 					</div>
 				</div>
+				<div className="clear-fix" ></div>
+				</div>
+			<Footer />
 			</div>
-		</div>
+		</>
 	);
 };
