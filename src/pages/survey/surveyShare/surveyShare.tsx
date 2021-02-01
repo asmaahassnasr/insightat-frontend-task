22464c6f38e14c2ed3dfe27b15c7300a0c3534e3
asmaahassnasr <asmaa14has@gmail.com>
2021-02-01 05:58:56 +0200

    Survey Share Page

--- src/pages/survey/surveyShare/surveyShare.tsx
+++ src/pages/survey/surveyShare/surveyShare.tsx
@@ -1,85 +1,49 @@
 import { SurveyNavBar } from '../components/surveyNavBar';
-import icon_arrow_down from './icon_arrow_down.svg';
-// import './surveyShare.css';
 export const SurveyShare = () => {
     return (
         <>
-            <SurveyNavBar />
-            <div className="container share-content">
-                <div className="caption"> 
-                    <h6>How do you want to share your survey?</h6>
+        <div className="survey-share">
+        <SurveyNavBar />
+        <hr className="survey-hr"></hr>
+        <div className="share-content">
+            <p className="share-ques">How do you want to share your survey?</p>
+            <div className="share-ways">
+                <div className="web-share">
+                    <p className="share-main-p">Web Link</p>
+                    <p className="share-second-p">Share it with people so they can visit and respond to your survey</p>
+                    <div className="share-generatedText"><span>https://loremipsum.com/random/a3ghtrtp</span></div>
+                    <button className="sign-up-for-free copy-btn">copy</button>
+                    <button className="share-btn">
+                    <i className="fas fa-arrow-down"></i>
+                         Download QR Code
+                         </button>
                 </div>
-
-                <div className="share-insde-content">
-                    <div className="web-link-box">
-                        <div className="web-link-box-caption">
-                            <span>Web Link</span>
-                            <span>Share it with people so they can visit and respond to your Survey</span>
-                        </div>
-                        <div className="link-container">
-                            <a href="#" className="lerom-link">https://loremipsum.com/random/a3ghtrtp</a>
-                            <a href="#" className="copy">copy</a>
-                            <button className="download"><img src={icon_arrow_down} alt="" />
-                                Download QR Code
-                            </button>
-                        </div>
-
+                <div className="other-share-ways">
+                    <div className="social-media-share">
+                        <p  className="share-main-p">Social media and email</p>
+                        <p className="share-second-p">Share your survey on social media or send it in an email</p>
+                        <div className="social-media-box">
+                            <span><i className="fa fa-facebook"></i></span>
+                            <span><i className="fa fa-linkedin"></i></span>
+                            <span><i className="fa fa-twitter"></i></span>
+                            <span><i className="fa fa-google"></i></span>
+                            <span><i className="fa fa-envelope"></i></span>
+                        </div> 
+                    </div>
+                    <div className="web-page-share">
+                        <p className="share-main-p">Embed in a web page</p>
+                        <p className="share-second-p">Paste this into your HTML wherever you want the embedded survey to appear on your page.</p>
+                        <button className="share-btn">Copy Code</button>
+                    </div>
+                    <div className="add-profile-share">
+                        <p  className="share-main-p">Add to Profile Page</p>
+                        <p  className="share-second-p">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.</p>
+                        <button className="share-btn">add</button>
                     </div>
                 </div>
-
-
-
-
-
-                    {/* <div className="share-insde-content">
-            <div className="web-link-box">
-                <div className="web-link-box-caption">
-                <span>Web Link</span>
-                <span>Share it with people so they can visit and respond to your Survey</span>
-                </div>
-                <div className="link-container">
-                    <a href="#" className="lerom-link ">https://loremipsum.com/random/a3ghtrtp</a>
-                    <a href="#" className="copy">copy</a>
-                    <UploadFile />
-                </div>       
-                </div>  
-                </div>
- */}
-
-                    <div className="box-container">
-                        <div className="socail-box first-box">
-                            <div className="socail-box-caption">
-                                <p>Social media and email</p>
-                                <p>Share your survey on social media or send it in an email</p>
-                            </div>
-                            <div className="icon-socaol-media-box">
-                                <span><i className="fa fa-facebook"></i></span>
-                                <span><i className="fa fa-linkedin"></i></span>
-                                <span><i className="fa fa-twitter"></i></span>
-                                <span><i className="fa fa-google"></i></span>
-                                <span><i className="fa fa-envelope"></i></span>
-                            </div>
-                        </div>
-                        <div className="socail-box second-box">
-                            <div className="socail-box-caption">
-                                <p>Embed in a web page</p>
-                                <p>Paste this into your HTML wherever you want the embedded survey to appear on your page.</p>
-                            </div>
-                            <div>
-                                <button className="Copy-Code">Copy Code</button>
-                            </div>
-                        </div>
-                        <div className="socail-box last-box">
-                            <div className="socail-box-caption">
-                                <p>Add to Profile Page</p>
-                                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.</p>
-                            </div>
-                            <div>
-                                <button className="Copy-Code">add</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            </div>
+        </div>
+        </div>
         </>
     )
 }
