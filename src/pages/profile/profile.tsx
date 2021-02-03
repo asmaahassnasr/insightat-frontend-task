7ac02b2f989d52f2bafaa38e77aed107255549b6
asmaahassnasr <asmaa14has@gmail.com>
2021-02-03 02:11:42 +0200

    Profile Page

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -1,79 +1,78 @@
 import React from 'react';
 import Header from './Header.png';
-import customer from './customer.png';
-import { ProfileNavBar} from './profileNavBar';
-import { NavLink } from "react-router-dom";
+import NIKE from './NIKE.png';
+import { ProfileNavBar } from './profileNavBar';
+import { Link, NavLink } from 'react-router-dom';
 import { Footer } from '../footer/footer';
-// import './profile.css';
 export const Profile = () => {
-    return (
-        <>
-        <div className="profile-container">
-        <ProfileNavBar />
-        </div>
-        {/* <div className="profile">
-            <div className="profile__image">
-            <img src={Header} />
-            <div className="profile__toggle"
-             style={{left: '70%'}}> 
-                <button
-                style={ {backgroundColor: '#fa7268',
-                    borderColor: '#fa7268',
-                    border: '1px solid #fa7268',
-                    color: 'white'}} >
-                Favourites
-                </button>
-                <NavLink className="profile__toggle__edit"
-                style={{backgroundColor: 'white',
-                borderColor: 'white',
-                border: '1px solid white',
-                color: '#343434'}} to="/editProfile">
-                <i className="fa fa-edit"></i>
-                </NavLink>
-            </div>
-            <div className="profile__logo-view">
-                <img src={customer}/>
-            </div>
-            </div>
-            <div className="profile__wrapper row">
-            <div className="profile__summary">
-                <h1 className="profile__summary__website text-center">
-                UserName 
-                </h1>
-                <p className="profile__summary__handle text-center">@userName.com </p>
-                <p className="profile__summary__description">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
-                 dolores eius obcaecati ad ipsa aliquid veniam vel molestiae odit unde labore nulla provident fugit assumenda voluptas,
-                 recusandae blanditiis. Voluptates, distinctio.
-                </p>
-                <div className="profile__summary__links">
-                <a className="profile__summary__link">
-                    <i className="fa fa-building"></i>
-                </a>
-                <div className="profile__summary__link">
-                    <i className="fa fa-facebook-square"></i>
-                </div>
-                <div className="profile__summary__link">
-                    <i className="fa fa-twitter"></i>
-                </div>
-                <div className="profile__summary__link">
-                    <i className="fa fa-linkedin"></i>
-                </div>
-                <div className="profile__summary__link">
-                    <i className="fa fa-google-plus"></i>
-                </div>
-                </div>
-            </div>
-            <div className="profile__content">
-                <p>User 1- Form</p>
-                <div
-                style={ {backgroundColor: '#000'} }
-                className="d-flex justify-content-center align-items-center preview"
-                >
-                </div>
-            </div>
-            </div>
-        </div> */}
-        </>
-    )
-}
+	return (
+		<div className="profile-page">
+			<div className="profile-container">
+				<ProfileNavBar />
+			</div>
+			<div className="profile-body">
+				<div className="profile-head-details">
+					<div className="profile-cover-img">
+						<img src={Header} />
+					</div>
+					<div className="profile-user-img">
+						<img src={NIKE} />
+					</div>
+					<div className="profile-buttons">
+						<Link to="" className="profile-fav-button">
+							Favoriets
+						</Link>
+						<Link to="" className="profile-edit-button">
+							<i className="fal fa-edit" />
+						</Link>
+					</div>
+				</div>
+				<div className="profile-body-details">
+					<div className="fl-left profile-user-data">
+						<p className="profile-user-name"> NIKE </p>
+						<p className="profile-user-userName"> @justdoit </p>
+						<p className="profile-user-caption">
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo ipsum, laborum repellat
+							repellendus magnam similique eaque modi! Nesciunt quo ut velit quaerat! Eum dolores
+							asperiores, iure animi at fugiat exercitationem.
+						</p>
+						<div className="profile-user-social">
+							<i className="fa fa-building" />
+							<i className="fa fa-facebook-square" />
+							<i className="fa fa-twitter" />
+							<i className="fa fa-linkedin" />
+							<i className="fa fa-google-plus" />
+						</div>
+					</div>
+					<div className="fl-right profile-user-questions">
+						<p>NIKE - Form</p>
+						<div className="profile-ques-container">
+							<form action="/action_page.php">
+								<div className="form-group">
+									<label htmlFor="text">
+										<span>1.</span> What is your name
+									</label>
+									<input type="text" className="form-control" placeholder=" What is your name" />
+								</div>
+								<div className="form-group">
+									<label htmlFor="text">
+										<span>2.</span> How old are you
+									</label>
+									<input type="text" className="form-control" placeholder=" How old are you" />
+								</div>
+								<div className="form-group">
+									<label htmlFor="email">
+										<span>3.</span> Your Email Please
+									</label>
+									<input type="email" className="form-control" placeholder=" Your Email Please" />
+								</div>
+							</form>
+						</div>
+					</div>
+					<div className="clear-fix" />
+				</div>
+			</div>
+			<Footer />
+		</div>
+	);
+};
